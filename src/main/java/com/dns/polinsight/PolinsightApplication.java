3cb6356e6fe4318c97e65ade5fa81f399a1f72e9
Chulwoong Choi <sentilemon02@gmail.com>
2021-05-31 12:11:34 +0900

    최초 프로젝트 생성

---
+++ src/main/java/com/dns/polinsight/PolinsightApplication.java
@@ -0,0 +1,13 @@
+package com.dns.polinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolinsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dns/polinsight/PolinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dns.polinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
