dabd056fdc3e31d9eaa91e4a405919994cc69d3f
AlBaneo93 <albaneo0724@gmail.com>
2021-06-02 01:16:38 +0900

    [+] add cors, csrf options [+] add loginprocessingurl, login successHandler, logoutUrl & successHandler

--- src/main/java/com/dns/polinsight/config/security/SecurityConfig.java
+++ src/main/java/com/dns/polinsight/config/security/SecurityConfig.java
@@ -1,6 +1,6 @@
-package com.dns.polinsight.config;
+package com.dns.polinsight.config.security;
 
-import com.dns.polinsight.service.UserServlce;
+import com.dns.polinsight.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,12 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final UserServlce service;
+  private final UserService service;
+
+  private final AuthenticationSuccessHandler successHandler;
+
+  private final LogoutSuccessHandler logoutSuccessHandler;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,19 +31,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     // @formatter:off
     http
+          .csrf().disable()
+          .cors().disable()
           .authorizeRequests()
-          .antMatchers("/static/**").permitAll()
-          .antMatchers("/login","/signup", "/index","/", "/404", "/signup").permitAll()
+          .antMatchers("/static/**").permitAll()  // 정적 리소스 접근 허가
+          .antMatchers("/login","/signup", "/index","/", "/404","loginSuccess" ).permitAll()
           .anyRequest().authenticated()
         .and()
           .formLogin()
-          .loginPage("/login")
-          .usernameParameter("email")
-          .passwordParameter("password")
-          .successForwardUrl("/index")
-          .failureForwardUrl("/signup")
+            .loginPage("/login")
+            .loginProcessingUrl("/dologin")
+            .usernameParameter("email")
+            .passwordParameter("pw")
+            .successHandler(successHandler)
+            .failureForwardUrl("/signup")
         .and()
-          .csrf().disable()
+            .logout()
+              .logoutUrl("/dologout")
+              .logoutSuccessHandler(logoutSuccessHandler)
 
 //          .oauth2Login()
 //        .and()
