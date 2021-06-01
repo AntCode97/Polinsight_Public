2f8c30be22d71a49202aeaaddb48920a32d61301
AlBaneo93 <albaneo0724@gmail.com>
2021-06-02 01:14:37 +0900

    [+] add signup logic

--- src/main/java/com/dns/polinsight/controller/UserController.java
+++ src/main/java/com/dns/polinsight/controller/UserController.java
@@ -1,24 +1,51 @@
 package com.dns.polinsight.controller;
 
-import com.dns.polinsight.service.UserServlce;
+import com.dns.polinsight.domain.User;
+import com.dns.polinsight.domain.UserRole;
+import com.dns.polinsight.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserServlce servlce;
+  private final UserService service;
+
+  private final PasswordEncoder passwordEncoder;
 
   @GetMapping("/login")
   public ModelAndView login() {
     ModelAndView mv = new ModelAndView();
     mv.setViewName("login");
     return mv;
+  }
+
+  @PostMapping("/signup")
+  public void userSignUp(User user, HttpServletResponse response) {
+    try {
+      service.save(User.builder()
+                       .email(user.getEmail())
+                       .password(passwordEncoder.encode(user.getPassword()))
+                       .name(user.getName())
+                       .roles(Collections.singletonList(UserRole.USER))  // TODO: 2021-06-01
+                       .build());
+
+      response.sendRedirect("/");
+    } catch (IOException e) {
+      e.printStackTrace();
+      // TODO: 2021-06-02 : Alert Error to user
+    }
   }
 
 }
