a4c7c60769fbd678497cab74e77c1dcea5a30d31
AlBaneo93 <albaneo0724@gmail.com>
2021-06-07 09:16:47 +0900

    [+] add survey service logic more detail

--- src/main/java/com/dns/polinsight/service/SurveyServiceImpl.java
+++ src/main/java/com/dns/polinsight/service/SurveyServiceImpl.java
@@ -1,13 +1,42 @@
 package com.dns.polinsight.service;
 
+import com.dns.polinsight.domain.Survey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SurveyServiceImpl implements SurveyService {
 
   private final MongoTemplate mongoTemplate;
+
+  @Override
+  public Survey save(Survey survey) {
+    return null;
+  }
+
+  @Override
+  public List<Survey> findAll() {
+    return null;
+  }
+
+  @Override
+  public Survey findById(Survey survey) {
+    return null;
+  }
+
+  @Override
+  public Optional<Survey> update(Survey survey) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void delete(Survey survey) {
+
+  }
 
 }
