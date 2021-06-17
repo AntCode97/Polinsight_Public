d4a9161afff4b491941711095854b8e3829a5b6b
AlBaneo93 <albaneo0724@gmail.com>
2021-06-17 20:17:21 +0900

    [+] add Redis Configuration & session storage config

--- src/main/java/com/dns/polinsight/config/RedisConfig.java
+++ src/main/java/com/dns/polinsight/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.dns.polinsight.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -9,9 +10,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//@Configuration
-// TODO: 2021-06-03 :설정 완료 해야함
-public class RedisConfig{
+@Configuration
+public class RedisConfig {
 
   @Value("${spring.redis.host}")
   public String host;
