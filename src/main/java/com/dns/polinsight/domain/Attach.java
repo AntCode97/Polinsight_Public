83b835e71f93e8ed369189bf56789521348028df
AlBaneo93 <albaneo0724@gmail.com>
2021-08-07 22:24:45 +0900

    [/] schema changed / [/] JPA Configuration change

--- src/main/java/com/dns/polinsight/domain/Attach.java
+++ src/main/java/com/dns/polinsight/domain/Attach.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class Attach {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
 
