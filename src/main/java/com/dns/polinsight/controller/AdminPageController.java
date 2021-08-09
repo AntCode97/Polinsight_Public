dda46130094233594038532416c33bde093b6938
AlBan <albaneo0724@gmail.com>
2021-08-09 18:44:12 +0900

    [+] survey info page

--- src/main/java/com/dns/polinsight/controller/AdminPageController.java
+++ src/main/java/com/dns/polinsight/controller/AdminPageController.java
@@ -1,12 +1,11 @@
 package com.dns.polinsight.controller;
 
+import com.dns.polinsight.domain.Survey;
 import com.dns.polinsight.service.SurveyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
@@ -38,42 +37,42 @@ public class AdminPageController {
   }
 
 
-//  /*
-//   * 게시글 관리 페이지
-//   * */
-//  @GetMapping("/post/manage")
-//  public ModelAndView getBoardManage() {
-//    return new ModelAndView("admin/admin_board_list");
-//  }
-//
-//  /*
-//   * 공지사항 관리 페이지
-//   * */
-//  @GetMapping("/noti/manage")
-//  public ModelAndView getNotiManage() {
-//    return new ModelAndView("admin/admin_board_register");
-//  }
-//
-//  @GetMapping("/user/manage")
-//  public ModelAndView getUserManage() {
-//    return new ModelAndView("admin/admin_board_view");
-//  }
-//
+  //  /*
+  //   * 게시글 관리 페이지
+  //   * */
+  //  @GetMapping("/post/manage")
+  //  public ModelAndView getBoardManage() {
+  //    return new ModelAndView("admin/admin_board_list");
+  //  }
+  //
+  //  /*
+  //   * 공지사항 관리 페이지
+  //   * */
+  //  @GetMapping("/noti/manage")
+  //  public ModelAndView getNotiManage() {
+  //    return new ModelAndView("admin/admin_board_register");
+  //  }
+  //
+  //  @GetMapping("/user/manage")
+  //  public ModelAndView getUserManage() {
+  //    return new ModelAndView("admin/admin_board_view");
+  //  }
+  //
   //
   //  @GetMapping("/posts/notice")
   //  public ModelAndView adminBoardnotice() {
   //    return new ModelAndView("admin/");
   //  }
 
-    @GetMapping("/boards/download")
-    public ModelAndView adminBoarddownload() {
-      return new ModelAndView("admin_post_download");
-    }
+  @GetMapping("/boards/download")
+  public ModelAndView adminBoarddownload() {
+    return new ModelAndView("admin_post_download");
+  }
 
-    @GetMapping("/boards/qna")
-    public ModelAndView adminBoardQnA() {
-      return new ModelAndView("admin_post_qna");
-    }
+  @GetMapping("/boards/qna")
+  public ModelAndView adminBoardQnA() {
+    return new ModelAndView("admin_post_qna");
+  }
 
   @GetMapping("/survey/{id}")
   public ModelAndView adminGetSurveyDetailInfo(@PathVariable("id") long surveyId) {
@@ -83,10 +82,11 @@ public class AdminPageController {
     return mv;
   }
 
-  @GetMapping("/surveyinfo")
-  public ModelAndView adminGetDetailPage(ModelAndView mav) {
-    mav.setViewName("admin/admin_survey_info");
-    return mav;
+  @GetMapping("/surveyinfo/{id}")
+  public ModelAndView adminGetDetailPage(@PathVariable("id") long surveyid) {
+    ModelAndView mv = new ModelAndView("admin/admin_survey_info");
+    mv.addObject("survey", surveyService.findSurveyById(surveyid).get());
+    return mv;
   }
 
   @GetMapping("/pointlist")
