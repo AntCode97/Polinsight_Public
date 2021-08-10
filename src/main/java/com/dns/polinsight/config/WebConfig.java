e042442cc7ce354d7a9460ea0fca9a0b55a58719
AlBaneo93 <albanseo0724@gmail.com>
2021-08-10 20:51:23 +0900

    [/] cors error modified

--- src/main/java/com/dns/polinsight/config/WebConfig.java
+++ src/main/java/com/dns/polinsight/config/WebConfig.java
@@ -26,7 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/**")
-        .addResourceLocations(resourceLocations);
+            .addResourceLocations(resourceLocations)
+            .addResourceLocations("*");
   }
 
 }
