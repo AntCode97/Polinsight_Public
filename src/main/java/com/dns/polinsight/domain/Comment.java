6256c6c571e1456dcec0c3e13587b6b7e2d98f25
AlBaneo93 <albaneo0724@gmail.com>
2021-10-12 11:07:09 +0900

    [/] shcmea validation error fix

--- src/main/java/com/dns/polinsight/domain/Comment.java
+++ src/main/java/com/dns/polinsight/domain/Comment.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public class Comment {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long seq;
 
   @Setter
